1ab550a9f7d23b7a82afa13063a252c828232ef4
Rahul <buriburiofficial@email.com>
2025-08-30 11:10:06 +0530

    UI polishing

--- qloset-api/src/main.ts
+++ qloset-api/src/main.ts
@@ -1,12 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
-
+/*
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
 app.enableCors({ origin: true, credentials: true });
 await app.listen(process.env.PORT || 3000, '0.0.0.0');
   console.log(`API running on http://localhost:${process.env.PORT || 3000}`);
+}
+bootstrap();
+*/
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors(); // or lock to your domains later
+  await app.listen(process.env.PORT ? Number(process.env.PORT) : 3000, '0.0.0.0');
 }
 bootstrap();
